677ffbbf04f2bfb1eaf45fff99d2b049be7d3c73
fersoftware <fersoftware@gmail.com>
2014-08-30 01:21:37 -0300

    Novo Projeto

--- fer.php
+++ fer.php
@@ -1,5 +1,20 @@
 <?php
-echo "teste";
-echo "<br>";
-?>
 
+require_once __DIR__."/vendor/autoload.php";
+
+$app = new Silex\Application();
+$app['debug'] = true;
+
+$app->register(new Silex\Provider\TwigServiceProvider(),array(
+    'twig.path' => __DIR__."/views"
+));
+
+$app->get("/ola/{nome}", function($nome) use ($app) {
+    return $app['twig']->render('ola.twig', array('nome'=> $nome));
+});
+
+$app->get("/register", function() use ($app) {
+    return $app['twig']->render('register.twig');
+});
+
+$app->run();
